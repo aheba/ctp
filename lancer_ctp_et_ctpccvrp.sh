6e67e276b0bfb0c45fde8941519668d935097d53
Maël Valais <mael65@gmail.com>
2015-04-08 21:01:33 +0200

    Correction de l'arrêt du script avant les affichages

--- lancer_ctp_et_ctpccvrp.sh
+++ lancer_ctp_et_ctpccvrp.sh
@@ -20,28 +20,30 @@ fi
 python parseur_noeuds.py --dat $1 > data_ctp.dat \
 	&& glpsol -m ctp.mod -d data_ctp.dat -y resultats_ctp.txt \
 	&& python parseur_noeuds.py --numeros --demandes --dot $1 resultats_ctp.txt > graphe_ctp.dot;
-res_ctp=$?
+if [ $? -ne 0 ]; then
+	echo "Erreur !"
+	exit 1
+fi
+
 python parseur_noeuds.py --dat $1 > data_ctpccvrp.dat \
 	&& glpsol -m ctpccvrp.mod -d data_ctpccvrp.dat -y resultats_ctpccvrp.txt \
 	&& python parseur_noeuds.py --numeros --demandes --dot $1 resultats_ctpccvrp.txt > graphe_ctpccvrp.dot
-res_ctpccvrp=$?
+if [ $? -ne 0 ]; then
+	echo "Erreur !"
+	exit 1
+fi
+
 
 echo "----------------------"
 echo "Résultats du CTP-VRP :"
 cat resultats_ctp.txt
+echo "----------------------"
 echo "Résultats du CTP-CCVRP :"
 cat resultats_ctpccvrp.txt
 echo "----------------------"
 echo "Résultats du CTP-VRP :"
-if [ $res_ctp -eq 0 ]; then
-	cat resultats_ctp.txt | head -3
-else
-	echo "PAS DE SOLUTION"	
-fi	
+cat resultats_ctp.txt | tail -4
+echo "----------------------"
 echo "Résultats du CTP-CCVRP :"
-if [ $res_ctpccvrp -eq 0 ]; then
-	cat resultats_ctpccvrp.txt | head -3
-else
-	echo "PAS DE SOLUTION"	
-fi	
+cat resultats_ctpccvrp.txt | tail -4
 
